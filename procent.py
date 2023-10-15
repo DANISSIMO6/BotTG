46b2bedae65f3d13d0312f84077e5844614a5583
DANISSIMO6 <147413579+DANISSIMO6@users.noreply.github.com>
2023-10-15 20:45:16 +0300

    Update procent.py
    2.5 сек между сообщениями и перезапуск кода в случае ошибки

--- procent.py
+++ procent.py
@@ -23,9 +23,10 @@ def commands(message):
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
 while True:
-    # Выполнение команды
-    # Создайте список тикеров
-    tickers = [
+    try:
+        # Выполнение команды
+        # Создайте список тикеров
+        tickers = [
         'ETLN', 'SBER', 'SBERP', 'ROSN', 'NVTK', 'LKOH', 'GAZP', 'SIBN', 'GMKN', 'SNGSP', 'LSNGP',
         'PLZL', 'TATN', 'TATNP', 'NLMK', 'SNGS', 'CHMF', 'TRNFP', 'UNAC', 'YNDX', 'PHOR', 'IRKT',
         'AKRN', 'TCSG', 'RUAL', 'MAGN', 'MGNT', 'OZON', 'ALRS', 'VSMO', 'PIKK', 'MTSS', 'MOEX',
@@ -37,93 +38,92 @@ while True:
         'DSKY', 'VRSB', 'TGKA', 'QIWI', 'MRKP', 'MRKU', 'GTRK', 'BLNG', 'MVID', 'CHMK', 'SVAV', 'ABRD',
         'KROT', 'LNZL', 'SFIN', 'TTLK', 'TGKN', 'MRKC', 'TGKBP', 'MRKY', 'UWGN', 'MRKV', 'PMSBP', 'TGKB',
         'OKEY', 'MRKZ', 'PMSB', 'PRFN', 'UNKL', 'KRKNP', 'LNZLP', 'CNTL', 'ROST', 'NSVZ', 'KLSB', 'CNTLP',
-        'LIFE', 'DIOD', 'RUGR'
-    ]
+        'LIFE', 'DIOD', 'RUGR']
 
-    # Преобразуйте список тикеров в строку, разделенную пробелами
-    tickers_str = ' '.join(tickers)
+        # Преобразуйте список тикеров в строку, разделенную пробелами
+        tickers_str = ' '.join(tickers)
 
-    # Измените команду на использование переменной tickers_str
-    command = f'tksbrokerapi --token "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ" --prices {tickers_str} --output C:\\Windows\\System32\\TKSBrokerAPI\\docs\\examples\\AnomalyVolumesDetector\\pricez.md'
+        # Измените команду на использование переменной tickers_str
+        command = f'tksbrokerapi --token "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ" --prices {tickers_str} --output C:\\Windows\\System32\\TKSBrokerAPI\\docs\\examples\\AnomalyVolumesDetector\\pricez.md'
 
-    subprocess.call(command, shell=True)
+        subprocess.call(command, shell=True)
 
-    # Чтение файла и создание DataFrame, пропуск первых 3 строк (заголовка и разделителей)
-    df = pd.read_csv('pricez.md', sep='|', skiprows=3, header='infer', skipinitialspace=True)
-    # Удаление лишних пробелов в именах столбцов
-    df.columns = df.columns.str.strip()
-    # Извлечение данных из столбца "Last price" и исключение первой строки
-    # Извлечение данных из столбца "Actual sell / buy" и исключение первой строки
-    actual_sell_buy = df['Actual sell / buy'].str.strip().iloc[1:]
+        # Чтение файла и создание DataFrame, пропуск первых 3 строк (заголовка и разделителей)
+        df = pd.read_csv('pricez.md', sep='|', skiprows=3, header='infer', skipinitialspace=True)
+        # Удаление лишних пробелов в именах столбцов
+        df.columns = df.columns.str.strip()
+        # Извлечение данных из столбца "Last price" и исключение первой строки
+        # Извлечение данных из столбца "Actual sell / buy" и исключение первой строки
+        actual_sell_buy = df['Actual sell / buy'].str.strip().iloc[1:]
 
-    # Разделение столбца на два столбца "Actual sell" и "Actual buy"
-    actual_sell_buy_split = actual_sell_buy.str.split('/', expand=True)
-    actual_sell = actual_sell_buy_split[0].str.strip()
-    actual_buy = actual_sell_buy_split[1].str.strip()
+        # Разделение столбца на два столбца "Actual sell" и "Actual buy"
+        actual_sell_buy_split = actual_sell_buy.str.split('/', expand=True)
+        actual_sell = actual_sell_buy_split[0].str.strip()
+        actual_buy = actual_sell_buy_split[1].str.strip()
 
-    def convert_to_float_or_none(x):
-        try:
-            return float(x)
-        except (ValueError, TypeError):
-            return None
+        def convert_to_float_or_none(x):
+            try:
+                return float(x)
+            except (ValueError, TypeError):
+                return None
 
 
-    actual_buy = actual_buy.apply(lambda x: convert_to_float_or_none(x)).round(4)
+        actual_buy = actual_buy.apply(lambda x: convert_to_float_or_none(x)).round(4)
 
-    # Преобразование цены покупки в строку и запись в файл
-    with open('example.txt', 'w') as example_file:
-        example_file.write(actual_buy.to_string(index=False, header=False))
+        # Преобразование цены покупки в строку и запись в файл
+        with open('example.txt', 'w') as example_file:
+            example_file.write(actual_buy.to_string(index=False, header=False))
 
-    # Чтение старых данных построчно из example1.txt
-    with open('example1.txt') as old_data_file:
-        old_data1 = old_data_file.readlines()
+        # Чтение старых данных построчно из example1.txt
+        with open('example1.txt') as old_data_file:
+            old_data1 = old_data_file.readlines()
 
-    # Чтение новых данных построчно из example.txt
-    with open('example.txt', 'r') as new_data_file:
-        new_data_lines = new_data_file.readlines()
+        # Чтение новых данных построчно из example.txt
+        with open('example.txt', 'r') as new_data_file:
+            new_data_lines = new_data_file.readlines()
 
-    # Чтение тикеров построчно из tickers.txt
-    with open('tickers.txt', 'r') as tickers_file:
-        tickers = tickers_file.readlines()
+        # Чтение тикеров построчно из tickers.txt
+        with open('tickers.txt', 'r') as tickers_file:
+            tickers = tickers_file.readlines()
 
-    # Проверка изменений построчно
-    for i, (old_line, new_line, ticker) in enumerate(zip(old_data1, new_data_lines, tickers)):
-        old_value = float(old_line.strip())
-        new_value = float(new_line.strip())
+        # Проверка изменений построчно
+        for i, (old_line, new_line, ticker) in enumerate(zip(old_data1, new_data_lines, tickers)):
+            old_value = float(old_line.strip())
+            new_value = float(new_line.strip())
 
-        if old_value != new_value:
-            print(f"Тикер: {ticker.strip()}")
-            print(f"Строка {i + 1}: Данные изменились!")
-            percentage_change = (new_value - old_value) / old_value * 100
-            print(f"Старые данные: {old_value}")
-            print(f"Новые данные: {new_value}")
-            print(f"Изменение в процентах: {percentage_change:.2f}%")
+            if old_value != new_value:
+                print(f"Тикер: {ticker.strip()}")
+                print(f"Строка {i + 1}: Данные изменились!")
+                percentage_change = (new_value - old_value) / old_value * 100
+                print(f"Старые данные: {old_value}")
+                print(f"Новые данные: {new_value}")
+                print(f"Изменение в процентах: {percentage_change:.2f}%")
 
-            # Проверка изменения в 0,5% и отправка уведомления
-            if abs(percentage_change) >= 5.0:
-                # Форматирование текста с использованием HTML-разметки
-                message_text = f"<b>Резкое изменение за 1 минуту:</b>\n\n" \
+                # Проверка изменения в 0,5% и отправка уведомления
+                if abs(percentage_change) >= 5.0:
+                    # Форматирование текста с использованием HTML-разметки
+                    message_text = f"<b>Резкое изменение за 1 минуту:</b>\n\n" \
                                f"<b>Тикер:</b> <code> {ticker.strip()}</code>\n" \
                                f"<b>Старые данные:</b> {old_value}\n" \
                                f"<b>Новые данные:</b> {new_value}\n" \
                                f"<b>Изменение в процентах:</b> {percentage_change:.2f}%"
 
-                # Отправляем уведомление в канал с форматированием
-                bot.send_message(channel_id, message_text, parse_mode="HTML")
-                time.sleep(3)
-        # Обновление старых данных в example1.txt
-        old_data1[i] = str(new_value) + '\n'
+                    # Отправляем уведомление в канал с форматированием
+                    bot.send_message(channel_id, message_text, parse_mode="HTML")
+                    time.sleep(2.5)
+            # Обновление старых данных в example1.txt
+            old_data1[i] = str(new_value) + '\n'
 
-    # Перезапись старых данных в example1.txt
-    with open('example1.txt', 'w') as old_data_file:
-        old_data_file.writelines(old_data1)
+        # Перезапись старых данных в example1.txt
+        with open('example1.txt', 'w') as old_data_file:
+            old_data_file.writelines(old_data1)
 
-    # Подождите 60 секунд перед следующей итерацией
-    time.sleep(60)
+        # Подождите 60 секунд перед следующей итерацией
+        time.sleep(60)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        print("Повторный запуск через 1 минуту...")
+        time.sleep(60)  # Ожидание 1 минуту перед повторным запуском
+
 
 bot.stop_polling()
-
-
-
-
-#6384593851:AAHHgUGXdQ8bar8HMKRuNgi1NnV_rhqnx0M
