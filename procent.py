2d2a4a64af747f104edd0380655027541c7d7765
DANISSIMO6 <147413579+DANISSIMO6@users.noreply.github.com>
2023-10-19 16:30:40 +0300

    Update procent.py
    Аномалии + Изменение цены

--- procent.py
+++ procent.py
@@ -2,6 +2,9 @@ import time
 import telebot
 import pandas as pd
 import subprocess
+
+import re  # Импорт модуля регулярных выражений
+
 from datetime import datetime, time as dt_time
 token = "6384593851:AAHHgUGXdQ8bar8HMKRuNgi1NnV_rhqnx0M"  # Замените на свой токен
 channel_id = "@novostikompaniy"  # Имя вашего канала
@@ -13,7 +16,8 @@ end_time1 = dt_time(10, 0)
 # Время начала и окончания второго отдыха (19:00 - 19:05)
 start_time2 = dt_time(19, 0)
 end_time2 = dt_time(19, 5)
-
+# Создайте список тикеров
+depth = 50  # Replace with the desired depth
 
 @bot.message_handler(content_types=['text'])
 def commands(message):
@@ -24,25 +28,20 @@ def commands(message):
 
 while True:
     try:
-        # Выполнение команды
-        # Создайте список тикеров
-        tickers = [
-        'ETLN', 'SBER', 'SBERP', 'ROSN', 'NVTK', 'LKOH', 'GAZP', 'SIBN', 'GMKN', 'SNGSP', 'LSNGP',
-        'PLZL', 'TATN', 'TATNP', 'NLMK', 'SNGS', 'CHMF', 'TRNFP', 'UNAC', 'YNDX', 'PHOR', 'IRKT',
-        'AKRN', 'TCSG', 'RUAL', 'MAGN', 'MGNT', 'OZON', 'ALRS', 'VSMO', 'PIKK', 'MTSS', 'MOEX',
-        'HYDR', 'BANE', 'IRAO', 'VTBR', 'FESH', 'FLOT', 'ENPG', 'BANEP', 'RTKM', 'SMLT', 'NMTP',
-        'CBOM', 'NKNC', 'RASP', 'POLY', 'RTKMP', 'KZOS', 'TRMK', 'LSNG', 'HHRU', 'GCHE', 'KMAZ',
-        'AFKS', 'MGTSP', 'AFLT', 'AGRO', 'FIVE', 'NKNCP', 'FEES', 'UPRO', 'KAZTP', 'APTK', 'MTLRP',
-        'BSPB', 'KAZT', 'MSNG', 'GLTR', 'YAKG', 'NKHP', 'MTLR', 'SGZH', 'AQUA', 'SELG', 'DVEC', 'MSTT',
-        'MRKS', 'INGR', 'BELU', 'LSRG', 'MSRS', 'OGKB', 'MDMG', 'KZOSP', 'RKKE', 'AMEZ', 'ROLO', 'RNFT',
-        'DSKY', 'VRSB', 'TGKA', 'QIWI', 'MRKP', 'MRKU', 'GTRK', 'BLNG', 'MVID', 'CHMK', 'SVAV', 'ABRD',
-        'KROT', 'LNZL', 'SFIN', 'TTLK', 'TGKN', 'MRKC', 'TGKBP', 'MRKY', 'UWGN', 'MRKV', 'PMSBP', 'TGKB',
-        'OKEY', 'MRKZ', 'PMSB', 'PRFN', 'UNKL', 'KRKNP', 'LNZLP', 'CNTL', 'ROST', 'NSVZ', 'KLSB', 'CNTLP',
-        'LIFE', 'DIOD', 'RUGR']
-
+        tickers = ['ETLN', 'SBER', 'SBERP', 'ROSN', 'NVTK', 'LKOH', 'GAZP', 'SIBN', 'GMKN', 'SNGSP', 'LSNGP',
+                'PLZL', 'TATN', 'TATNP', 'NLMK', 'SNGS', 'CHMF', 'TRNFP', 'UNAC', 'YNDX', 'PHOR', 'IRKT',
+                'AKRN', 'TCSG', 'RUAL', 'MAGN', 'MGNT', 'OZON', 'ALRS', 'VSMO', 'PIKK', 'MTSS', 'MOEX',
+                'HYDR', 'BANE', 'IRAO', 'VTBR', 'FESH', 'FLOT', 'ENPG', 'BANEP', 'RTKM', 'SMLT', 'NMTP',
+                'CBOM', 'NKNC', 'RASP', 'POLY', 'RTKMP', 'KZOS', 'TRMK', 'LSNG', 'HHRU', 'GCHE', 'KMAZ',
+                'AFKS', 'MGTSP', 'AFLT', 'AGRO', 'FIVE', 'NKNCP', 'FEES', 'UPRO', 'KAZTP', 'APTK', 'MTLRP',
+                'BSPB', 'KAZT', 'MSNG', 'GLTR', 'YAKG', 'NKHP', 'MTLR', 'SGZH', 'AQUA', 'SELG', 'DVEC', 'MSTT',
+                'MRKS', 'INGR', 'BELU', 'LSRG', 'MSRS', 'OGKB', 'MDMG', 'KZOSP', 'RKKE', 'AMEZ', 'ROLO', 'RNFT',
+                'DSKY', 'VRSB', 'TGKA', 'QIWI', 'MRKP', 'MRKU', 'GTRK', 'BLNG', 'MVID', 'CHMK', 'SVAV', 'ABRD',
+                'KROT', 'LNZL', 'SFIN', 'TTLK', 'TGKN', 'MRKC', 'TGKBP', 'MRKY', 'UWGN', 'MRKV', 'PMSBP', 'TGKB',
+                'OKEY', 'MRKZ', 'PMSB', 'PRFN', 'UNKL', 'KRKNP', 'LNZLP', 'CNTL', 'ROST', 'NSVZ', 'KLSB', 'CNTLP',
+                'LIFE', 'DIOD', 'RUGR']
         # Преобразуйте список тикеров в строку, разделенную пробелами
         tickers_str = ' '.join(tickers)
-
         # Измените команду на использование переменной tickers_str
         command = f'tksbrokerapi --token "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ" --prices {tickers_str} --output C:\\Windows\\System32\\TKSBrokerAPI\\docs\\examples\\AnomalyVolumesDetector\\pricez.md'
 
@@ -55,7 +54,6 @@ while True:
         # Извлечение данных из столбца "Last price" и исключение первой строки
         # Извлечение данных из столбца "Actual sell / buy" и исключение первой строки
         actual_sell_buy = df['Actual sell / buy'].str.strip().iloc[1:]
-
         # Разделение столбца на два столбца "Actual sell" и "Actual buy"
         actual_sell_buy_split = actual_sell_buy.str.split('/', expand=True)
         actual_sell = actual_sell_buy_split[0].str.strip()
@@ -66,7 +64,6 @@ while True:
                 return float(x)
             except (ValueError, TypeError):
                 return None
-
 
         actual_buy = actual_buy.apply(lambda x: convert_to_float_or_none(x)).round(4)
 
@@ -84,14 +81,31 @@ while True:
 
         # Чтение тикеров построчно из tickers.txt
         with open('tickers.txt', 'r') as tickers_file:
-            tickers = tickers_file.readlines()
-
+            tickers = tickers_file.read().splitlines()
         # Проверка изменений построчно
         for i, (old_line, new_line, ticker) in enumerate(zip(old_data1, new_data_lines, tickers)):
             old_value = float(old_line.strip())
             new_value = float(new_line.strip())
+            order_book_command = f'tksbrokerapi --token "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ" -t {ticker} --depth {depth} --price > glass.md'
+            subprocess.call(order_book_command, shell=True)
+            # Читаем данные из файла glass.md
+            with open("glass.md", "r") as file:
+                file_contents = file.read()
+                print(file_contents)
 
-            if old_value != new_value:
+                # Используем регулярные выражения для извлечения числа из строки "Total sell"
+                total_sell_match = re.search(r"Total sell:\s*(\d+)", file_contents)
+                if total_sell_match:
+                    total_sell = int(total_sell_match.group(1))
+                else:
+                    total_sell = None
+                    # Используем регулярные выражения для извлечения числа из строки "Total buy"
+                total_buy_match = re.search(r"Total buy:\s*(\d+)", file_contents)
+                if total_buy_match:
+                    total_buy = int(total_buy_match.group(1))
+                else:
+                    total_buy = None
+
                 print(f"Тикер: {ticker.strip()}")
                 print(f"Строка {i + 1}: Данные изменились!")
                 percentage_change = (new_value - old_value) / old_value * 100
@@ -100,14 +114,18 @@ while True:
                 print(f"Изменение в процентах: {percentage_change:.2f}%")
 
                 # Проверка изменения в 0,5% и отправка уведомления
-                if abs(percentage_change) >= 5.0:
+                #if abs(percentage_change) >= 5.0:
                     # Форматирование текста с использованием HTML-разметки
-                    message_text = f"<b>Резкое изменение за 1 минуту:</b>\n\n" \
+                if total_sell and total_buy:
+                    buy_ratio = total_buy / (total_sell + total_buy) * 100
+                    sell_ratio = total_sell / (total_sell + total_buy) * 100
+                    if buy_ratio > 10 or sell_ratio > 10:
+                        message_text = f"<b>Резкое изменение за 1 минуту:</b>\n\n" \
                                f"<b>Тикер:</b> <code> {ticker.strip()}</code>\n" \
                                f"<b>Старые данные:</b> {old_value}\n" \
                                f"<b>Новые данные:</b> {new_value}\n" \
-                               f"<b>Изменение в процентах:</b> {percentage_change:.2f}%"
-
+                               f"<b>Изменение в процентах:</b> {percentage_change:.2f}%\n" \
+                                       f"Аномалия обнаружена для {ticker.strip()}: Покупка {sell_ratio:.2f}% Продажа {buy_ratio:.2f}%"
                     # Отправляем уведомление в канал с форматированием
                     bot.send_message(channel_id, message_text, parse_mode="HTML")
                     time.sleep(2.5)
