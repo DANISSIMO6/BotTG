f30a43e01752fb1da02b6480611427d78c09e357
DANISSIMO6 <147413579+DANISSIMO6@users.noreply.github.com>
2023-10-24 15:34:49 +0300

    Update Stream_client.py

--- Stream_client.py
+++ Stream_client.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import warnings
 from tinkoff.invest import (
     AsyncClient,
@@ -9,42 +8,40 @@ from tinkoff.invest import (
     SubscriptionAction,
     SubscriptionInterval,
 )
-import pytz
-
 TOKEN = "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ"
+# Список FIGIs
+FIGIs = [
+    "BBG001M2SC01"
+]
 
 async def main():
+    data = []
+
     async def request_iterator():
-        yield MarketDataRequest(
-            subscribe_candles_request=SubscribeCandlesRequest(
-                subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
-                instruments=[
-                    CandleInstrument(
-                        figi="BBG004730N88",
-                        interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
-                    )
-                ],
+        for figi in FIGIs:
+            yield MarketDataRequest(
+                subscribe_candles_request=SubscribeCandlesRequest(
+                    subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                    instruments=[
+                        CandleInstrument(
+                            figi=figi,
+                            interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+                        )
+                    ],
+                )
             )
-        )
         while True:
             await asyncio.sleep(1)
 
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     async with AsyncClient(TOKEN) as client:
         async for marketdata in client.market_data_stream.market_data_stream(
-            request_iterator()
+                request_iterator()
         ):
-            if marketdata and marketdata.candle:
-                candle = marketdata.candle
-                figi = candle.figi
-                open_quotation = f"{candle.open.units},{candle.open.nano}"
-                close_quotation = f"{candle.close.units},{candle.close.nano}"
-                volume = candle.volume
-                time = candle.time.astimezone(pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S')
-                last_trade_ts = candle.last_trade_ts.astimezone(pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+            print(marketdata)
+            with open('Stream.md', 'a') as stream:
+                stream.write(str(marketdata) + '\n')
 
-                with open('Stream.md', 'a') as f:
-                    f.write(f"FIGI: {figi}, Open: {open_quotation}, Close: {close_quotation}, Volume: {volume}, Time: {time}, Last Trade Time: {last_trade_ts}\n")
 
 if __name__ == "__main__":
     asyncio.run(main())
