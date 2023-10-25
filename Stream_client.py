fe29e4e845e3757bcdbc1fe1cace70f06d1ff687
DANISSIMO6 <147413579+DANISSIMO6@users.noreply.github.com>
2023-10-25 18:15:44 +0300

    Update Stream_client.py

--- Stream_client.py
+++ Stream_client.py
@@ -1,66 +1,65 @@
-import re
-import telebot
-from datetime import datetime, timedelta, timezone
-import time
+import asyncio
+import warnings
+from tinkoff.invest import (
+    AsyncClient,
+    CandleInstrument,
+    MarketDataRequest,
+    SubscribeCandlesRequest,
+    SubscriptionAction,
+    SubscriptionInterval,
+)
+TOKEN = "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ"
+# Список FIGIs
+FIGIs = [
+    "BBG001M2SC01", "BBG004730N88", "BBG0047315Y7", "BBG004731354", "BBG00475KKY8", "BBG004731032", "BBG004730RP0",
+    "BBG004S684M6", "BBG004731489", "BBG004S681M2", "BBG000NLCCM3", "BBG000R607Y3", "BBG004RVFFC0", "BBG004S68829",
+    "BBG004S681B4", "BBG0047315D0", "BBG00475K6C3", "BBG00475KHX6", "BBG000Q7ZZY2", "BBG006L8G4H1", "BBG004S689R0",
+    "BBG000FWGSZ5", "BBG004S688G4", "BBG00QPYJ5H0", "BBG008F2T3T2", "BBG004S68507", "BBG004RVFCY3", "BBG00Y91R9T3",
+    "BBG004S68B31", "BBG004S68CV8", "BBG004S68BH6", "BBG004S681W1", "BBG004730JJ5", "BBG00475K2X9", "BBG004S68758",
+    "BBG004S68473", "BBG004730ZJ9", "BBG000QF1Q17", "BBG000R04X57", "BBG000RMWQD4", "BBG004S686N0", "BBG004S682Z6",
+    "BBG00F6NKQX3", "BBG004S68BR5", "BBG009GSYN76", "BBG000GQSRR5", "BBG004S68696", "BBG004PYF2N3", "BBG004S685M3",
+    "BBG0029SFXB3", "BBG004TC84Z8", "BBG000NLC9Z6", "TCS2207L1061", "BBG000RTHVK7", "BBG000LNHHJ9", "BBG004S68614",
+    "BBG000PZ0833", "BBG004S683W7", "BBG007N0Z367", "BBG00JXPFBN0", "BBG000GQSVC2", "BBG00475JZZ6", "BBG004S686W0",
+    "BBG002B9T6Y1", "BBG000K3STR7", "BBG004S68FR6", "BBG000QJW156", "BBG002B9MYC1", "BBG004S687W8", "BBG000VFX6Y4",
+    "BBG002B298N6", "BBG00BGKYH17", "BBG004S68598", "BBG0100R9963", "BBG000W325F7", "BBG002458LF8", "BBG000V07CB8",
+    "BBG004S68DD6", "BBG000VJMH65", "BBG000BXPZB7", "BBG000TY1CD1", "BBG004S68C39", "BBG004S687G6", "BBG000RK52V1",
+    "BBG00Y3XYV94", "BBG0029SG1C1", "BBG000LWNRP3", "BBG000RJWGC4", "BBG004Z2RGW8", "BBG00F9XX7H4", "BBG000BN56Q9",
+    "BBG000BX7DH0", "BBG000QFH687", "BBG005D1WCQ1", "BBG000VG1034", "BBG000VKG4R5", "BBG00HY6V6H5", "BBG000VQWH86",
+    "BBG004S68CP5", "BBG000RP8V70", "BBG004S68JR8", "BBG002W2FT69", "BBG000NLB2G3", "BBG000SK7JS5", "BBG003LYCMB1",
+    "BBG000RJL816", "BBG000RG4ZQ4", "BBG000VH7TZ8", "BBG000Q7GJ60", "BBG000C7P5M7", "BBG008HD3V85", "BBG000PKWCQ7",
+    "BBG000MZL2S9", "BBG000Q7GG57", "BBG00172J7S9", "BBG000TJ6F42", "BBG000MZL0Y6", "TCS00A0JNXF9", "BBG002YFXL29",
+    "BBG002B2J5X0", "BBG000SR0YS4", "BBG000BBV4M5", "BBG000BSBZH7", "BBG002BCQK67", "BBG000DBD6F6", "BBG0027F0Y27",
+    "BBG0019K04R5", "BBG000G25P51", "BBG0014PFYM2"
+]
 
-sent_messages = set()
-# Создайте объект timezone для UTC
-utc_timezone = timezone.utc
-stream_data = []
-# Ваш токен бота и ID чата
-bot_token = "6384593851:AAHHgUGXdQ8bar8HMKRuNgi1NnV_rhqnx0M"
-chat_id = "@novostikompaniy"
+async def main():
+    data = []
 
-# Создание экземпляра бота
-bot = telebot.TeleBot(bot_token)
+    async def request_iterator():
+        for figi in FIGIs:
+            yield MarketDataRequest(
+                subscribe_candles_request=SubscribeCandlesRequest(
+                    subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                    instruments=[
+                        CandleInstrument(
+                            figi=figi,
+                            interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_FIVE_MINUTES,
+                        )
+                    ],
+                )
+            )
+        while True:
+            await asyncio.sleep(1)
 
-# Создание словаря для хранения Total Volume по FIGI
-total_volumes = {}
-last_cleanup_time = datetime.now(timezone.utc)
-while True:
-    # Считывание данных из файла FIGI.md и заполнение словаря
-    with open("FIGI.md", "r") as figi_file:
-        figi_data = figi_file.read()
-        figi_matches = re.finditer(r"FIGI: (\S+)[\s\S]*?Total Volume for 2nd to 5th weeks after division: ([\d.]+)", figi_data)
-        for match in figi_matches:
-            figi = match.group(1)
-            total_volume = float(match.group(2))
-            total_volumes[figi] = total_volume
-
-    # Считывание данных из файла Stream.md
-    with open("Stream.md", "r") as stream_file:
-        lines = stream_file.readlines()
-        new_lines = []
-        for line in lines:
-            figi_match = re.search(r"figi='(\S+)'", line)
-            volume_match = re.search(r"volume=(\d+)", line)
-            time_match = re.search(r"time=datetime.datetime\(([\d, ]+), tzinfo=datetime.timezone.utc\)", line)
-            close_match = re.search(r"close=Quotation\(units=(\d+), nano=(\d+)\)", line)
-            if figi_match and volume_match and time_match and close_match:
-                figi = figi_match.group(1)
-                volume = int(volume_match.group(1))
-                close_units = int(close_match.group(1))
-                close_nano = int(close_match.group(2))
-                close_value = close_units + close_nano * 1e-9  # Преобразование в одно число
-                total_value = volume * close_value  # Умножение объема на цену закрытия
-                line_time = datetime(*map(int, time_match.group(1).split(", ")), tzinfo=utc_timezone)
-                current_time = datetime.now(timezone.utc)
-                if figi in total_volumes and volume > (total_volumes[figi] * 5) and figi not in sent_messages:
-                    # Отправка сообщения в Telegram, если объем в 2 раза больше Total Volume
-                    message = f"Объем для FIGI {figi} в Stream.md больше чем в 2 раза Total Volume: {volume}. " \
-                              f"Общая стоимость составила: {total_value}"
-                    bot.send_message(chat_id, message)
-                    sent_messages.add(figi)
-                if current_time - line_time < timedelta(minutes=6):
-                    new_lines.append(line)
-        with open("Stream.md", "w") as stream_file:
-            stream_file.writelines(new_lines)
-
-    if current_time - last_cleanup_time >= timedelta(minutes=1):
-        sent_messages.clear()  # Очищаем множество отправленных сообщений
-        last_cleanup_time = current_time  # Обновляем время последней очистки
-
-    # Пауза на 1 минуту перед следующей итерацией
-    time.sleep(2.5)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    async with AsyncClient(TOKEN) as client:
+        async for marketdata in client.market_data_stream.market_data_stream(
+                request_iterator()
+        ):
+            print(marketdata)
+            with open('Stream.md', 'a') as stream:
+                stream.write(str(marketdata) + '\n')
 
 
+if __name__ == "__main__":
+    asyncio.run(main())
