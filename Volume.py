0e9dc4ca69d5de6c3afd43f82c6561c5f09560af
DANISSIMO6 <147413579+DANISSIMO6@users.noreply.github.com>
2023-10-26 16:03:58 +0300

    Update Volume.py

--- Volume.py
+++ Volume.py
@@ -3,6 +3,19 @@ import telebot
 from datetime import datetime, timedelta, timezone
 import time
 
+import asyncio
+from tinkoff.invest import (
+    AsyncClient,
+    CandleInstrument,
+    MarketDataRequest,
+    SubscribeCandlesRequest,
+    SubscriptionAction,
+    SubscriptionInterval,
+    SubscribeOrderBookRequest,
+    OrderBookInstrument,
+)
+TOKEN = "t.OpTGgjrL00hW6AruBxEr9vhtxNd2gWxmVJ8uE2qEex-i699xS8C4PhGpyASIQbiL6U3Z109SsnEOHO7xQ5HgYQ"
+
 sent_messages = set()
 # Создайте объект timezone для UTC
 utc_timezone = timezone.utc
@@ -14,53 +27,78 @@ chat_id = "@novostikompaniy"
 # Создание экземпляра бота
 bot = telebot.TeleBot(bot_token)
 
-# Создание словаря для хранения Total Volume по FIGI
-total_volumes = {}
-last_cleanup_time = datetime.now(timezone.utc)
-while True:
-    # Считывание данных из файла FIGI.md и заполнение словаря
-    with open("FIGI.md", "r") as figi_file:
-        figi_data = figi_file.read()
-        figi_matches = re.finditer(r"FIGI: (\S+)[\s\S]*?Total Volume for 2nd to 5th weeks after division: ([\d.]+)", figi_data)
-        for match in figi_matches:
-            figi = match.group(1)
-            total_volume = float(match.group(2))
-            total_volumes[figi] = total_volume
+async def request_iterator(figi):
+    yield MarketDataRequest(
+        subscribe_order_book_request=SubscribeOrderBookRequest(
+            subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+            instruments=[
+                OrderBookInstrument(
+                    figi=figi,
+                    depth=50,
+                )
+            ],
+        )
+    )
+    await asyncio.sleep(1)
+async def process_marketdata(figi):
+    async with AsyncClient(TOKEN) as client:
+        async for marketdata in client.market_data_stream.market_data_stream(
+                request_iterator(figi)
+        ):
+            print(marketdata)
+            with open('StreamBook.md', 'a') as stream:
+                stream.write(str(marketdata) + '\n')
 
-    # Считывание данных из файла Stream.md
-    with open("Stream.md", "r") as stream_file:
-        lines = stream_file.readlines()
-        new_lines = []
-        for line in lines:
-            figi_match = re.search(r"figi='(\S+)'", line)
-            volume_match = re.search(r"volume=(\d+)", line)
-            time_match = re.search(r"time=datetime.datetime\(([\d, ]+), tzinfo=datetime.timezone.utc\)", line)
-            close_match = re.search(r"close=Quotation\(units=(\d+), nano=(\d+)\)", line)
-            if figi_match and volume_match and time_match and close_match:
-                figi = figi_match.group(1)
-                volume = int(volume_match.group(1))
-                close_units = int(close_match.group(1))
-                close_nano = int(close_match.group(2))
-                close_value = close_units + close_nano * 1e-9  # Преобразование в одно число
-                total_value = volume * close_value  # Умножение объема на цену закрытия
-                line_time = datetime(*map(int, time_match.group(1).split(", ")), tzinfo=utc_timezone)
-                current_time = datetime.now(timezone.utc)
-                if figi in total_volumes and volume > (total_volumes[figi] * 2) and figi not in sent_messages:
-                    # Отправка сообщения в Telegram, если объем в 2 раза больше Total Volume
-                    message = f"Объем для FIGI {figi} в Stream.md больше чем в 2 раза Total Volume: {volume}. " \
-                              f"Общая стоимость составила: {total_value}"
-                    bot.send_message(chat_id, message)
-                    sent_messages.add(figi)
-                if current_time - line_time < timedelta(minutes=6):
-                    new_lines.append(line)
-        with open("Stream.md", "w") as stream_file:
-            stream_file.writelines(new_lines)
+async def main():
+    last_cleanup_time = datetime.now(timezone.utc)
+    # Создание словаря для хранения Total Volume по FIGI
+    total_volumes = {}
+    while True:
+        # Считывание данных из файла FIGI.md и заполнение словаря
+        with open("FIGI.md", "r") as figi_file:
+            figi_data = figi_file.read()
+            figi_matches = re.finditer(r"FIGI: (\S+)[\s\S]*?Total Volume for 2nd to 5th weeks after division: ([\d.]+)", figi_data)
+            for match in figi_matches:
+                figi = match.group(1)
+                total_volume = float(match.group(2))
+                total_volumes[figi] = total_volume
 
-    if current_time - last_cleanup_time >= timedelta(minutes=1):
-        sent_messages.clear()  # Очищаем множество отправленных сообщений
-        last_cleanup_time = current_time  # Обновляем время последней очистки
+        # Считывание данных из файла Stream.md
+        with open("Stream.md", "r") as stream_file:
+            lines = stream_file.readlines()
+            new_lines = []
+            for line in lines:
+                figi_match = re.search(r"figi='(\S+)'", line)
+                volume_match = re.search(r"volume=(\d+)", line)
+                time_match = re.search(r"time=datetime.datetime\(([\d, ]+), tzinfo=datetime.timezone.utc\)", line)
+                close_match = re.search(r"close=Quotation\(units=(\d+), nano=(\d+)\)", line)
+                if figi_match and volume_match and time_match and close_match:
+                    figi = figi_match.group(1)
+                    volume = int(volume_match.group(1))
+                    close_units = int(close_match.group(1))
+                    close_nano = int(close_match.group(2))
+                    close_value = close_units + close_nano * 1e-9  # Преобразование в одно число
+                    total_value = volume * close_value  # Умножение объема на цену закрытия
+                    line_time = datetime(*map(int, time_match.group(1).split(", ")), tzinfo=utc_timezone)
+                    current_time = datetime.now(timezone.utc)
+                    if figi in total_volumes and volume > (total_volumes[figi] * 34) and figi not in sent_messages:
+                        await process_marketdata(figi)
+                        # Отправка сообщения в Telegram, если объем в 2 раза больше Total Volume
+                        message = f"Объем для FIGI {figi} в Stream.md больше чем в 2 раза Total Volume: {volume}. " \
+                                  f"Общая стоимость составила: {total_value}"
+                        bot.send_message(chat_id, message)
+                        sent_messages.add(figi)
+                    if current_time - line_time < timedelta(minutes=6):
+                        new_lines.append(line)
+            with open("Stream.md", "w") as stream_file:
+                stream_file.writelines(new_lines)
 
-    # Пауза на 1 минуту перед следующей итерацией
-    time.sleep(2.5)
+        if current_time - last_cleanup_time >= timedelta(minutes=1):
+            sent_messages.clear()  # Очищаем множество отправленных сообщений
+            last_cleanup_time = current_time  # Обновляем время последней очистки
 
+        # Пауза на 1 минуту перед следующей итерацией
+        time.sleep(2.5)
 
+if __name__ == "__main__":
+    asyncio.run(main())
