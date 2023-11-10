b9bb5e5eab68acc06c86ba45b11fc9cbf323526a
Vampir423 <147413190+Vampir423@users.noreply.github.com>
2023-11-10 19:04:22 +0300

    Update парсинг.py

--- парсинг.py
+++ парсинг.py
@@ -1,18 +1,15 @@
 import telebot
 import requests
 import time
-
 from bs4 import BeautifulSoup
 
-token = "6384593851:AAHHgUGXdQ8bar8HMKRuNgi1NnV_rhqnx0M"  # ыВаш токен
-channel_id = "@novostikompaniy"  # Ваш логин канала
+token = "6384593851:AAHHgUGXdQ8bar8HMKRuNgi1NnV_rhqnx0M"  # Your token
+channel_id = "@novostikompaniy"  # Your channel username
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['text'])
 def commands(message):
-    #bot.send_message(channel_id, message.text)
     if message.text == "Старт":
-        # bot.send_message(channel_id, "Hello")
         back_silkainfa_href = None
         while True:
             silkainfa_text = parser(back_silkainfa_href)
@@ -24,29 +21,30 @@ def commands(message):
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
-
 def parser(back_silkainfa_href):
     URL = 'https://www.e-disclosure.ru'
     page = requests.get(URL)
     soup = BeautifulSoup(page.text, 'html.parser')
 
     table = soup.find('div', class_="table table_full last30__table table_top-events-list")
-    #post = table.find ('tr')
     infa = table.find('br')
     silkainfa = table.find('a', href_="", href=True)
     silkainfa_href = silkainfa['href']
 
     if silkainfa_href != back_silkainfa_href:
-        #time = table.find('td').text.strip()
         title = table.find('a', href_="").text.strip()
         silka = table.find('a', href_="", href=True)["href"].strip()
         infa = table.find('br')
         titleinfa = infa.find('a', href_="").text.strip()
         silkainfa = infa.find('a', href_="", href=True)["href"].strip()
-        return f"{title}\n\n{silka}\n\n{titleinfa}\n\n{silkainfa}", silkainfa_href
 
+        # Open the link and get data
+        event_page = requests.get(silkainfa)
+        event_soup = BeautifulSoup(event_page.text, 'html.parser')
+        event_data = event_soup.find('div', style="word-break: break-word; word-wrap: break-word; white-space: pre-wrap;").text.strip()
+
+        return f"{title}\n\n{silka}\n\n{titleinfa}\n\n{silkainfa}\n\n{event_data}", silkainfa_href
     else:
         return None, silkainfa_href
 
 bot.polling()
-
